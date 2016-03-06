c6083a07d40385d25e448c65e28999c19adfbda4
GladOSkar <okkathe97th@gmail.com>
2016-03-06 15:25:28 +0100

    Lots of new stuff

--- script.js
+++ script.js
@@ -38,17 +38,17 @@ var database = {
 var english = {
 	records: "RECORDS",
 	about: "ABOUT US",
-	lorem: "LOREM IPSUM",
 	contact: "CONTACT",
-	location: "LOCATION"
+	location: "LOCATION",
+	abouttext: "We are a little Record Store in Berlin-Schöneberg. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }
 
 var german = {
 	records: "PLATTEN",
 	about: "ÜBER UNS",
-	lorem: "LOREM IPSUM",
 	contact: "KONTAKT",
-	location: "LAGE"
+	location: "LAGE",
+	abouttext: "Wir sind ein kleiner Schallplattenladen in Berlin-Schöneberg. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }
 
 var lang = english;

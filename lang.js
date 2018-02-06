115057d899fe158084e545b7a73a19ef6105b371
GladOSkar <okkathe97th@gmail.com>
2018-02-06 13:18:43 +0100

    updated texts

--- lang.js
+++ lang.js
@@ -10,23 +10,21 @@ var langen = {
 	favsongs: "OUR FAVORITE SONGS",
 	lang: "SEITE AUF DEUTSCH",
 	opening: "OPENING TIMES",
-	newstext: "	<i>3/20/2017</i><br> \
-                <h2>WE'RE STILL HERE!</h2> \
-                And we have a new website!<br><br> \
-				That’s the good news.<br><br> \
-				The maybe less good news is:<br><br> \
-				We won’t be here forever (as a lot of people were assuming).<br><br> \
-				Sometime in early 2018 MR DEAD & MRS FREE will close this door forever after 35 years in business.<br><br> \
-				So come here while you can, there are lots of special offers and we are selling parts of our own collections, too.<br><br> \
+	newstext: "	<i>11/12/2017</i><br> \
+				<h2>Final Days</h2> \
+				Our last day of business will be <b>February 3rd</b>.<br><br> \
+				After that, you can come to our<br><br> \
+				<b>Farewell Party</b><br><br> \
+				on the <b>10th of Febuary</b> from <b>3:00 PM to 8:00 or 9:00 PM</b>.<br><br> \
 				<h2>~</h2> \
-				<i>11/12/2017</i><br> \
-				<h2>Relax</h2> \
-				We will still be here in January.<br><br> \
-				Our last day of business will be <b>February 3rd, 2018</b>.<br><br> \
-				So there’s still time to browse through our collections.",
+				<i>1/1/2018</i><br> \
+				<h2>Best Of List 2017</h2> \
+                The yearly (and last) best of list by Ina and Volker is here!<br><br> \
+				You can find it <a href=\"Top15_2017.pdf\">here</a>.",
 	gosearch: "Search our inventory...",
 	newstuffpdf: "New in stock [pdf]",
 	treasures: "Treasures of which we part [pdf]",
+	bestof: "Best of list 2017 [pdf]",
 	abouttext: "MR DEAD & MRS FREE have been selling records for 34 years now.<br><br> \
 				We sold records before there were CDs or MPs3s or streaming.<br><br> \
 				And even in the darkest days of the 90s when CDs seemed to be the only medium in the market, we have always had at least 50% of our turnover with Vinyl.<br><br> \
@@ -50,28 +48,25 @@ var langde = {
 	favsongs: "UNSERE LIEBLINGSSONGS",
 	lang: "PAGE IN ENGLISH",
 	opening: "ÖFFUNGSZEITEN",
-	newstext: "	<i>20.3.2017</i><br> \
-				<h2>ES GIBT UNS NOCH!</h2> \
-                Und wir haben wieder eine Webseite!!<br><br> \
-				Das ist die gute Nachricht.<br><br> \
-				Die vielleicht nicht ganz so gute:<br><br> \
-				Es wird uns doch nicht ewig geben... (was ja viele Leute gehofft haben).<br><br> \
-				Anfang 2018 werden MR DEAD & MRS FREE voraussichtlich nach 35 Jahren den Laden dichtmachen.<br><br> \
-				Also kommt her, solange wir noch da sind. Es gibt jede Menge Sonderangebote und wir verkaufen auch Teile unserer eigenen Sammlungen.<br><br> \
+	newstext: "	<i>31.1.2018</i><br> \
+				<h2>ENDSPURT!!!</h2> \
+				Nach all den Jahren nur noch 3 Tage offen.<br><br> \
+				Schon ein seltsames Gefühl.<br><br> \
+				Am 3.2. gehen hier zum letzten Mal Platten über den Tresen.<br><br> \
+				Und kommt – es sind immer noch welche da! Aber nicht mehr viele.<br><br> \
+				Und wer danach immer noch Sehnsucht hat, kommt eine Woche später, am <b>10.2.</b> zu unserer<br><br> \
+				<b>ABSCHIEDSPARTY</b><br><br> \
+				Samstagnachmittag ab <b>15 Uhr bis 19 oder 20 Uhr</b>.<br><br> \
+				Und gerne eine Platte mitbringen, die Ihr irgendwann hier gekauft habt, und die Euch wichtig ist.<br><br> \
 				<h2>~</h2> \
-				<i>18.11.2017</i><br> \
-				<h2>Liebe Kunden</h2> \
-				weil Ihr uns jetzt am Wochenende immer die Bude einrennt, und weil bald Weihnachten ist, verlängern wir unsere Öffnungszeiten:<br><br> \
-				Am <b>Samstag</b> ist ab <b>Dezember</b> immer bis <b>18 Uhr</b> geöffnet.<br><br> \
-				<h2>~</h2> \
-				<i>11.12.2017</i><br> \
-				<h2>Keine Panik!</h2> \
-				Wir sind noch bis zum <b>3.2.2018</b> für Euch da.<br><br> \
-				Wir freuen uns natürlich auch jetzt schon über Eure Geschenke + all das liebende Angedenken.<br><br> \
-				Aber ein bisschen Zeit hat’s noch...",
+				<i>1.1.2018</i><br> \
+				<h2>Best of Liste 2017</h2> \
+                Die Jährliche (und letzte) Best of Liste von Ina und Volker ist da!<br><br> \
+				Ihr findet sie <a href=\"Top15_2017.pdf\">hier</a>.",
 	gosearch: "Durchsuchen Sie unser Lager...",
 	newstuffpdf: "Neuheiten im Sortiment [pdf]",
 	treasures: "Schätze von denen wir uns trennen [pdf]",
+	bestof: "Best of Liste 2017 [pdf]",
 	abouttext: "MR DEAD & MRS FREE verkaufen Schallplatten seit 34 Jahren.<br><br> \
 				Als es noch keine CDs und MP3s und Streaming gab (und auch noch fast keine Computer und kein Internet... dafür Karteikarten und Fax-Geräte).<br><br> \
 				Als es in den 90ern – fast – nur noch CDs gab,  haben wir immer mindestens 50% vom Umsatz mit Schallplatten gemacht. Das kleine gallische Dorf des Vinyls in einem Meer von Silberlingen.<br><br> \
